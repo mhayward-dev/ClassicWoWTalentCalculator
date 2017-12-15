69700da7a79c13aa8a9a6e6bc61c960790a574f7
Mark <mhayward10@gmail.com>
2017-12-15 16:38:48 +0000

    Initial About page

---
+++ VanillaReborn/Controllers/AboutController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VanillaReborn.Views
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.CurrentPage = "About";
+            return View();
+        }
+    }
+}
