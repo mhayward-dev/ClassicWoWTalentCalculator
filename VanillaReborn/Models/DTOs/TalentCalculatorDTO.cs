173df545cadcbbb6971058ec55f287e572842d8d
Mark <mhayward10@gmail.com>
2017-12-22 12:50:14 +0000

    New DTO for TalentCalculator

---
+++ VanillaReborn/Models/DTOs/TalentCalculatorDTO.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using VanillaReborn.Models.Enums;
+using Frameworks.Extensions;
+
+namespace VanillaReborn.Models.DTOs
+{
+    public class TalentCalculatorDTO
+    {
+        public WarcraftClassDTO SelectedClass { get; set; }
+        public int TotalTalentCount { get; set; }
+        public Dictionary<int, string> ArrowDirections { get; set; }
+
+        public static TalentCalculatorDTO ToDTO(WarcraftClass wc)
+        {
+            var totalTalentCount = 0;
+            wc.WarcraftClassSpecifications?.ForEach(wcs => totalTalentCount += wcs.Talents.Count());
+
+            return new TalentCalculatorDTO
+            {
+                SelectedClass = WarcraftClassDTO.ToDTO(wc),
+                TotalTalentCount = totalTalentCount,
+                ArrowDirections = EnumExtensions.ToDictionary<RequirementArrowDirections>()
+            };
+        }
+    }
+}
